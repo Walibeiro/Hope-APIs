df775e9fc821c80480d466fb452f8be15ca5a891
Walibeiro <Walison@pcjv.de>
2018-09-05 13:53:16 +0200

    Fixed error in ECMA.Math.pas

--- ECMA/ECMA.Math.pas
+++ ECMA/ECMA.Math.pas
@@ -3,7 +3,14 @@ unit ECMA.Math;
 type
   JMath = class external 'math'
     function random: Float;
+    E: Float;
+    LN10: Float;
+    LN2: Float;
+    LOG2E: Float;
+    PI: Float;
+    SQRT1_2: Float;
+    SQRT2: Float;
   end;
 
 var
-  Math external 'math': JMath;
+  Math external 'Math': JMath;
